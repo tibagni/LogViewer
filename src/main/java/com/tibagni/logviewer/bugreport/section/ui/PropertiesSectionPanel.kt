80430895be35892051925a6fc489551d4e8c594f
Tiago Bagni <tibagni@gmail.com>
2021-02-15 22:25:05 -0300

    minor UI adjustments

--- src/main/java/com/tibagni/logviewer/bugreport/section/ui/PropertiesSectionPanel.kt
+++ src/main/java/com/tibagni/logviewer/bugreport/section/ui/PropertiesSectionPanel.kt
@@ -1,23 +1,19 @@
 package com.tibagni.logviewer.bugreport.section.ui
 
+import com.tibagni.logviewer.ServiceLocator
 import com.tibagni.logviewer.bugreport.section.PropertiesSection
 import com.tibagni.logviewer.util.layout.GBConstraintsBuilder
 import com.tibagni.logviewer.view.maxWidthOfColumn
+import java.awt.Color
+import java.awt.Component
+import java.awt.Font
 import java.awt.GridBagConstraints
+import javax.swing.BorderFactory
 import javax.swing.JScrollPane
 import javax.swing.JTable
-import javax.swing.SwingUtilities
 import javax.swing.table.DefaultTableModel
-import javax.swing.table.TableCellRenderer
-import kotlin.math.max
-import java.awt.Color
 
-import javax.swing.UIManager
-
-import javax.swing.UIDefaults
-
-
-
+import javax.swing.table.DefaultTableCellRenderer
 
 
 class PropertiesSectionPanel(private val section: PropertiesSection) : SectionPanel(section.sectionName) {
@@ -34,12 +30,14 @@ class PropertiesSectionPanel(private val section: PropertiesSection) : SectionPa
       arrayOf("Uptime", section.uptime)
     )
     val propTable = JTable()
-    propTable.model = PropValueTableModel(data, arrayOf("Property", "Value"))
+    propTable.model = PropTableModel(data, arrayOf("Property", "Value"))
+    propTable.setDefaultRenderer(Object::class.java, PropTableRenderer())
+
     // Resize the first column according to its maximum width
     val propColWidth = propTable.maxWidthOfColumn(0)
     propTable.columnModel.getColumn(0).minWidth = propColWidth * 1.5.toInt()
     propTable.columnModel.getColumn(0).maxWidth = propColWidth * 3
-    propTable.columnModel.getColumn(0).preferredWidth  = propColWidth * 2
+    propTable.columnModel.getColumn(0).preferredWidth = propColWidth * 2
 
     propTable.tableHeader.resizingAllowed = true
     add(
@@ -55,6 +53,24 @@ class PropertiesSectionPanel(private val section: PropertiesSection) : SectionPa
   }
 }
 
-private class PropValueTableModel(data: Array<Array<String>>, cols: Array<String>): DefaultTableModel(data, cols) {
+private class PropTableModel(data: Array<Array<String>>, cols: Array<String>) : DefaultTableModel(data, cols) {
   override fun isCellEditable(row: Int, column: Int) = false
+}
+
+private class PropTableRenderer : DefaultTableCellRenderer() {
+  private val borderColor = if (ServiceLocator.themeManager.isDark) Color.GRAY else Color.DARK_GRAY
+  override fun getTableCellRendererComponent(
+    table: JTable?,
+    value: Any?,
+    isSelected: Boolean,
+    hasFocus: Boolean,
+    row: Int,
+    column: Int
+  ): Component {
+    super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column)
+    border = BorderFactory.createMatteBorder(0, 0, 1, 0, borderColor)
+    val fontStyle = if (column == 0) Font.BOLD else Font.ITALIC
+    font = Font(font.name, fontStyle, font.size)
+    return this
+  }
 }
